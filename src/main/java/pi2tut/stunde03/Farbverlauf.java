8162872fd67cc8b3934d37b0ec78395aafe61f5b
vexplained <98122708+vexplained@users.noreply.github.com>
2026-05-06 22:19:33 +0200

    Fix stunde03: duplicate main method

--- src/main/java/pi2tut/stunde03/Farbverlauf.java
+++ src/main/java/pi2tut/stunde03/Farbverlauf.java
@@ -3,33 +3,23 @@ package pi2tut.stunde03;
 import java.awt.Color;
 
 public class Farbverlauf {
-	private static int colorToRGB(float colorPercent)
-	{
+	private static int colorToRGB(float colorPercent) {
 		return Color.HSBtoRGB((float) colorPercent, 1, 1);
 	}
 
-	public static int createColor()
-	{
+	public static int createColor() {
 		return 0x39E4FF;
 	}
 
-	public static int[] createColorArray()
-	{
+	public static int[] createColorArray() {
 		return null;
 	}
 
-	public static int[][] createColorArray2()
-	{
+	public static int[][] createColorArray2() {
 		return null;
 	}
 
-	static void main()
-	{
-		GUI.main(null);
-	}
-	
-	static void main()
-	{
+	static void main() {
 		GUI.main(null);
 	}
 }
